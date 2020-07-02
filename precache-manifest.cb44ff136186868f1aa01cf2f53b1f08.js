d9d62e925b368eb1fa9f5723a56fcc750feabf7e
IliyanaVladimirova <iliqna_vladimirova@abv.bg>
2020-07-03 01:10:38 +0300

    Updates

---
+++ precache-manifest.cb44ff136186868f1aa01cf2f53b1f08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c95f332c22484714aa44cf45900797a",
+    "url": "/goodreads-lite/index.html"
+  },
+  {
+    "revision": "cbffd6ada6ba2e2e2e27",
+    "url": "/goodreads-lite/static/css/main.ba3ac0bc.chunk.css"
+  },
+  {
+    "revision": "5fb2c7f00aff6d9b3d74",
+    "url": "/goodreads-lite/static/js/2.4bb430c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goodreads-lite/static/js/2.4bb430c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbffd6ada6ba2e2e2e27",
+    "url": "/goodreads-lite/static/js/main.a16c4dc9.chunk.js"
+  },
+  {
+    "revision": "7d336c67554bc88c6801",
+    "url": "/goodreads-lite/static/js/runtime-main.e8d0708f.js"
+  },
+  {
+    "revision": "2f3141f15e0c9f4c76f9ed4c72f08cd7",
+    "url": "/goodreads-lite/static/media/masthead_background.2f3141f1.jpg"
+  }
+]);
